7780c065450a58aa5ab0c69dfaf10601d04571bf
Manuel <manuel.buchauer@student.uibk.ac.at>
2024-03-03 09:38:16 +0100

    mod(gsn-core): mod UploadCommandData for commands

--- gsn-core/src/main/java/ch/epfl/gsn/networking/zeromq/UploadCommandData.java
+++ gsn-core/src/main/java/ch/epfl/gsn/networking/zeromq/UploadCommandData.java
@@ -3,23 +3,26 @@ package ch.epfl.gsn.networking.zeromq;
 
 public class UploadCommandData {
     private String vsname;
+    private String cmd;
     private String[] paramNames;
     private String[] paramValues;
 
     public UploadCommandData() {
         this.vsname = "";
+        this.cmd = "";
         this.paramNames = new String[0];
         this.paramValues = new String[0];
     }
 
-    public UploadCommandData(String vsname, String[] paramNames, String[] paramValues) {
+    public UploadCommandData(String vsname, String cmd, String[] paramNames, String[] paramValues) {
         this.vsname = vsname;
+        this.cmd = cmd;
         this.paramNames = paramNames;
         this.paramValues = paramValues;
     }
 
     public String toString(){
-        StringBuilder s =new StringBuilder().append("UploadcommandData for "+ this.vsname+ "\n");
+        StringBuilder s =new StringBuilder().append("UploadcommandData for "+ this.vsname+ ":" + this.cmd + "\n");
         for (int i = 0; i < paramNames.length; i++) {
             s.append("Param Name: " + paramNames[i] + ", Param Value: " + paramValues[i] +"\n");
         }
@@ -31,6 +34,14 @@ public class UploadCommandData {
 
     public void setVsname(String vsname) {
         this.vsname = vsname;
+    }
+
+    public String getCmd() {
+        return cmd;
+    }
+
+    public void setCmd(String cmd) {
+        this.cmd = cmd;
     }
 
     public String[] getParamNames() {
