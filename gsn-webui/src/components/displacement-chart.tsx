e60fa8fec4a8d59ee16a8afa7aa91ab263dd3c59
Manuel <manuel.buchauer@student.uibk.ac.at>
2024-07-11 17:50:39 +0200

    modify(gsn-webui): modify timeseries chart, displacement chart and dashboard

--- gsn-webui/src/components/displacement-chart.tsx
+++ gsn-webui/src/components/displacement-chart.tsx
@@ -25,29 +25,40 @@ const opts: uPlot.Options = {
   ],
 };
 
-async function fetchChartData() {
-  const response = await fetch(`http://walker.uibk.ac.at:3000/api/displacement`);
+async function fetchChartData(resolution: string) {
+
+  const response = await fetch(`http://localhost:3000/api/postgres?resolution=${resolution}`);
+
   if (!response.ok) {
     throw new Error("Error fetching data");
   }
-  const data = await response.json();
-  const count = data.count;
+  var data = await response.json();
+  data = data.rows;
+
+  const count = data.length;
   const chartData: [number[], number[]] = [Array(count), Array(count)];
 
   for (let i = 0; i < count; i++) {
-    chartData[0][i] = new Date(data.dataset[i][0]).getTime();
-    chartData[1][i] = data.dataset[i][1];
+    chartData[0][i] = new Date(Number(data[i]['monthly'])).getTime();
+    chartData[1][i] = data[i]['avg'];
   }
+
   return chartData;
 }
 
-const DataComponent = ({ width, resolution }: { width: number,resolution:string }) => {
+const DataComponent = ({ width, resolution }: { width: number, resolution: string }) => {
+
   const { data } = useSuspenseQuery({
-    queryKey: ["displacement"],
-    queryFn: fetchChartData,
+    queryKey: ["displacement", resolution],
+    queryFn: () => fetchChartData(resolution),
   });
 
   const chartRef = useRef<uPlot | null>(null);
+  const [zoomLevel, setZoomLevel] = useState({ min: null, max: null });
+
+  const resetZoomLevel = () => {
+    setZoomLevel({ min: null, max: null });
+  };
 
   useEffect(() => {
     if (chartRef.current) {
@@ -56,51 +67,88 @@ const DataComponent = ({ width, resolution }: { width: number,resolution:string 
   }, [width]);
 
   useEffect(() => {
-    opts.hooks = {
-      init: [
-        (u) => {
-          u.over.ondblclick = (e) => {
-            u.setData(data);
-          };
-        },
-      ],
-      setSelect: [
-        async (u) => {
-          if (u.select.width > 0) {
-            let min = u.posToVal(u.select.left, "x");
-            let max = u.posToVal(u.select.left + u.select.width, "x");
+    const u = chartRef.current;
+    if (!u) return;
 
-            const resp = await fetch(
-              `http://walker.uibk.ac.at:3000/api/displacement?startTime=${min}&endTime=${max}`,
-            );
+    u.over.ondblclick = (e) => {
+      u.setData(data);
+      resetZoomLevel(); // Reset zoom level on double-click
+    };
+  }, [data, resetZoomLevel]);
 
-            const newData = await resp.json();
+  useEffect(() => {
+    const u = chartRef.current;
+    if (!u) return;
 
-            const count = newData.count;
-            const chartData: [number[], number[]] = [
-              Array(count),
-              Array(count),
-            ];
+    const handleZoomAndFetch = async () => {
+      if (u.select.width > 0) {
+        let min = u.posToVal(u.select.left, "x");
+        let max = u.posToVal(u.select.left + u.select.width, "x");
 
-            for (let i = 0; i < count; i++) {
-              chartData[0][i] = new Date(newData.dataset[i][0]).getTime();
-              chartData[1][i] = newData.dataset[i][1];
-            }
-            // set new data
-            u.setData(chartData, false);
+        setZoomLevel({ min, max });
 
-            // zoom to selection
-            u.setScale("x", { min, max });
+        const resp = await fetch(
+          `http://localhost:3000/api/postgres?startTime=${min}&endTime=${max}&resolution=${resolution}`
+        );
 
-            // reset selection
-            u.setSelect({ width: 0, height: 0 }, false);
+        var newData = await resp.json();
+        newData = newData.rows;
+
+        const count = newData.length;
+        const chartData: [number[], number[]] = [Array(count), Array(count)];
+
+        for (let i = 0; i < count; i++) {
+          chartData[0][i] = new Date(Number(newData[i]['monthly'])).getTime();
+          chartData[1][i] = newData[i]['avg'];
+        }
+        // set new data
+        u.setData(chartData, false);
+
+        // zoom to selection
+        u.setScale("x", { min, max });
+
+        // reset selection
+        u.setSelect({ width: 0, height: 0 }, false);
+      } else {
+        const { min, max } = zoomLevel;
+
+        if (min !== null && max !== null) {
+
+          const resp = await fetch(
+            `http://localhost:3000/api/postgres?startTime=${min}&endTime=${max}&resolution=${resolution}`
+          );
+
+          var newData = await resp.json();
+          newData = newData.rows;
+
+          const count = newData.length;
+          const chartData: [number[], number[]] = [Array(count), Array(count)];
+
+          for (let i = 0; i < count; i++) {
+            chartData[0][i] = new Date(Number(newData[i]['monthly'])).getTime();
+            chartData[1][i] = newData[i]['avg'];
           }
-        },
-      ],
+          // set new data
+          u.setData(chartData, false);
+
+          // zoom to selection
+          u.setScale("x", { min, max });
+        }
+      }
     };
 
-    return () => {};
-  }, []);
+    u.hooks.setSelect = [handleZoomAndFetch];
+
+    // Apply the zoom level when it's not null
+    const { min, max } = zoomLevel;
+    if (min !== null && max !== null) {
+      u.setScale("x", { min, max });
+    }
+
+    return () => {
+      u.hooks.setSelect = [];
+    };
+  }, [resolution, zoomLevel]);
 
   return (
     <>
@@ -114,6 +162,7 @@ const DataComponent = ({ width, resolution }: { width: number,resolution:string 
     </>
   );
 };
+
 
 export function ChartContainer({resolution}:{resolution:string}) {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -160,6 +209,7 @@ const LoadingSpinner = () => {
 export default function DisplacementChart() {
   const { reset } = useQueryErrorResetBoundary();
   const [resolution, setResolution] = useState("");
+
   return (
     <div className="border rounded p-2 bg-white h-[477px]">
       <div className="flex items-center justify-between px-1 -mb-1">
--- gsn-webui/src/components/timeseries-chart.tsx
+++ gsn-webui/src/components/timeseries-chart.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { Suspense, useEffect, useRef, useState } from "react";
+import React, { Suspense, useEffect, useRef, useState, useMemo } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import UplotReact from "uplot-react";
 import "uplot/dist/uPlot.min.css";
@@ -10,110 +10,235 @@ import {
 } from "@tanstack/react-query";
 import { Button } from "./ui/button";
 import { DataSelector } from "./data-selector";
+import { Sensor } from "@/lib/sensor-utils";
+import uPlot from "uplot";
+import ChartMenu from "./chart-menu";
+import { CardTitle } from "./ui/card";
 
-const opts: uPlot.Options = {
+
+const initialOpts: uPlot.Options = {
   title: "Matterhorn Temperaturechain",
   width: 800,
   height: 600,
   ms: 1,
-  series: [
-    {},
-    {
-      label: "temperature_nearsurface_t1",
-      stroke: "red",
-      value: (_: any, rawValue: number) => (rawValue ? rawValue + " °C" : ""),
-    },
-  ],
+  series: [{}],
+  axes: [{}],
 };
 
-async function fetchChartData({ pageParam }: { pageParam: string }) {
-  const response = await fetch(`http://walker.uibk.ac.at:3000/api/temperature-rock`);
-  // const response = await fetch(`http://walker.uibk.ac.at:3000/api/temperature_rock?dataRow=${pageParam}`);
-  if (!response.ok) {
-    throw new Error("Error fetching data");
+function hashStringToColor(str) {
+  if (!str) {
+    return '#000000'; // Return black color for empty or undefined strings
   }
-  const data = await response.json();
-  const count = data.count;
-  const chartData: [number[], number[]] = [Array(count), Array(count)];
 
-  for (let i = 0; i < count; i++) {
-    chartData[0][count - i - 1] = new Date(data.dataset[i][0]).getTime();
-    chartData[1][count - i - 1] = data.dataset[i][1];
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
   }
-  return chartData;
+
+  let color = '#';
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += ('00' + value.toString(16)).slice(-2);
+  }
+
+  return color;
 }
 
-const DataComponent = ({ width }: { width: number }) => {
+async function fetchChartData({ pageParam, selectedSensor, resolution }: { pageParam: string, selectedSensor: Sensor, resolution: string }) {
+
+  const chartDataMap = new Map();
+
+  if (selectedSensor) {
+    const numericFieldTypes = ["DOUBLE", "INTEGER", "BIGINT", "SMALLINT"];
+    const numericFields = selectedSensor.fields.filter(field => numericFieldTypes.includes(field.type));
+    const fieldsString = numericFields.map(field => field.name).join(',');
+
+    const response = await fetch(`http://localhost:3000/api/timeseries?sensorName=${selectedSensor.name}&startTime=${selectedSensor.startTime}&endTime=${selectedSensor.endTime}&fields=${fieldsString}&resolution=${resolution}`);
+
+    if (!response.ok) {
+      throw new Error("Error fetching data");
+    }
+    let data = await response.json();
+    data = data.rows;
+
+    const count = data.length;
+
+    chartDataMap.set('timed', Array(count).fill(0));
+    numericFields.forEach(field => {
+      chartDataMap.set(field.name, Array(count).fill(0));
+    });
+
+    for (let i = 0; i < count; i++) {
+      chartDataMap.get('timed')[i] = new Date(Number(data[i]['monthly'])).getTime();
+      numericFields.forEach(field => {
+        chartDataMap.get(field.name)[i] = parseFloat(data[i][field.name]) || 0;
+      });
+    }
+
+    return chartDataMap;
+  }
+  return chartDataMap;
+}
+
+const DataComponent = ({ width, selectedSensor, resolution }: { width: number, selectedSensor: Sensor, resolution: string }) => {
+
+  if (!selectedSensor) {
+    return null;
+  }
+  
+  const numericFieldTypes = ["DOUBLE", "INTEGER", "BIGINT", "SMALLINT"];
+  // Memoize numeric fields to avoid re-calculation on every render
+  const numericFields = useMemo(() => selectedSensor.fields.filter(field => numericFieldTypes.includes(field.type)), [selectedSensor]);
+  
+  var chartRef = useRef<uPlot | null>(null);
   const queryClient = useQueryClient();
-  const { data, fetchNextPage } = useSuspenseInfiniteQuery({
-    queryKey: ["chartData"],
-    queryFn: fetchChartData,
-    getNextPageParam: (lastPage, pages) => "temperature_nearsurface_t2",
-    initialPageParam: "temperature_nearsurface_t1",
+  
+  const { data } = useSuspenseInfiniteQuery({
+    queryKey: ["chartData", selectedSensor.name, resolution],
+    queryFn: ({ pageParam }) => fetchChartData({ pageParam, selectedSensor, resolution }),
+    getNextPageParam: (lastPage, pages) => lastPage.length > 0 ? "next" : undefined,
+    initialPageParam: "start",
   });
 
-  const chartRef = useRef<uPlot | null>(null);
-
-  const [selectedValues, setSelectedValues] = useState<string[]>([
-    "temperature_nearsurface_t1",
-  ]);
+  var [selectedValues, setSelectedValues] = useState<string[]>([]);
+  const [zoomLevel, setZoomLevel] = useState({ min: null, max: null });
 
   useEffect(() => {
     if (chartRef.current) {
       chartRef.current.setSize({ width, height: 600 });
     }
-  }, [width]);
+  }, [width, chartRef]);
 
   useEffect(() => {
-    if (chartRef.current) {
-      chartRef.current.setData([
-        data.pages[0][0],
-        ...data.pages.map((page) => page[1]),
-      ]);
+    setSelectedValues(numericFields.map(field => field.name));
+  }, [selectedSensor, numericFields]);
+
+  const chartOptions = useMemo(() => {
+    const series = [{ label: 'timed', stroke: 'black' }];
+    const axes = [{}];
+
+    selectedValues.forEach((field, index) => {
+      const color = hashStringToColor(field);
+      series.push({ label: field, stroke: color, scale: field });
+      if (index === 0) {
+        //axes.push({ values: data.pages[0]?.get('timed') });
+      }
+    });
+
+    initialOpts.width = width;
+    initialOpts.title = selectedSensor.name;
+
+    return {
+      ...initialOpts,
+      series,
+      axes,
+    };
+  }, [selectedValues, data]);
+
+  const handleAddRow = (row: string) => {
+    setSelectedValues(prevSelectedValues => [...prevSelectedValues, row]);
+  };
+
+  const handleRemoveRow = (row: string) => {
+    setSelectedValues(prevSelectedValues => prevSelectedValues.filter(value => value !== row));
+  };
+
+  const handleResetDataRows = () => setSelectedValues([]);
+  
+  const resetZoomLevel = () => {
+    setZoomLevel({ min: null, max: null });
+  };
+
+  useEffect(() => {
+    const u = chartRef.current;
+    if (!u) return;
+    u.over.ondblclick = (e) => {
+      const valuesArray = Array.from(data.pages[0].values());
+      u.setData(valuesArray);
+      resetZoomLevel(); // Reset zoom level on double-click
+    };
+  }, [data, resetZoomLevel]);
+
+  useEffect(() => {
+
+    const u = chartRef.current;
+
+    if (!u) return;
+    const handleZoomAndFetch = async () => {
+      
+      if (u.select.width > 0) {
+
+        const chartDataMap = new Map();
+
+        let min = u.posToVal(u.select.left, "x");
+        let max = u.posToVal(u.select.left + u.select.width, "x");
+
+        const fieldsString = selectedValues.map(field => field).join(',');
+
+        setZoomLevel({ min, max });
+
+        const resp = await fetch(
+          `http://localhost:3000/api/timeseries?sensorName=${selectedSensor.name}&startTime=${min}&endTime=${max}&fields=${fieldsString}&resolution=${resolution}`
+        );
+
+        let data = await resp.json();
+        data = data.rows;
+
+        const count = data.length;
+    
+        chartDataMap.set('timed', Array(count).fill(0));
+        selectedValues.forEach(field => {
+          chartDataMap.set(field, Array(count).fill(0));
+        });
+    
+        for (let i = 0; i < count; i++) {
+          chartDataMap.get('timed')[i] = new Date(Number(data[i]['monthly'])).getTime();
+          selectedValues.forEach(field => {
+            chartDataMap.get(field)[i] = parseFloat(data[i][field]) || 0;
+          });
+        }
+
+        const valuesArray = Array.from(chartDataMap.values());
+
+        // set new data
+        u.setData(valuesArray, false);
+
+        // zoom to selection
+        u.setScale("x", { min, max });
+
+        // reset selection
+        u.setSelect({ width: 0, height: 0 }, false);
+      }
+    };
+
+    u.hooks.setSelect = [handleZoomAndFetch];
+
+    // Apply the zoom level when it's not null
+    const { min, max } = zoomLevel;
+    if (min !== null && max !== null) {
+      u.setScale("x", { min, max });
     }
-  }, [data.pages.length, data.pages]);
+
+    return () => {
+      u.hooks.setSelect = [];
+    };
+  }, [selectedSensor.name, resolution, zoomLevel, selectedValues]);
+
 
   return (
     <>
       <div className="my-2">
         <DataSelector
-          dataRows={[
-            "temperature_nearsurface_t1",
-            "temperature_nearsurface_t2",
-            "temperature_5cm",
-            "temperature_10cm",
-            "temperature_20cm",
-            "temperature_30cm",
-          ]}
+          dataRows={numericFields.map(field => field.name)}
           selectedValues={selectedValues}
-          onAddRow={(row) => {
-            setSelectedValues([...selectedValues, row]);
-            fetchNextPage();
-            chartRef.current?.addSeries(
-              {
-                label: row,
-                stroke: "blue",
-                value: (_: any, rawValue: number) =>
-                  rawValue ? rawValue + " °C" : "",
-              },
-              data.pages.length,
-            );
-          }}
-          onRemoveRow={(row) => {
-            setSelectedValues(selectedValues.filter((v) => v !== row));
-            queryClient.setQueryData(["chartData"], (da) => ({
-              pages: (da as any).pages.slice(0, 1),
-              pageParams: (da as any).pageParams.slice(0, 1),
-            }));
-
-            chartRef.current?.delSeries(1);
-          }}
-          resetDataRows={() => setSelectedValues([])}
+          onAddRow={handleAddRow}
+          onRemoveRow={handleRemoveRow}
+          resetDataRows={handleResetDataRows}
         />
       </div>
       <UplotReact
-        options={opts}
-        data={[]}
+        options={chartOptions}
+        data={chartOptions.series.map(series => data.pages[0]?.get(series.label) || [])}
         onCreate={(chart) => {
           chartRef.current = chart;
         }}
@@ -122,7 +247,8 @@ const DataComponent = ({ width }: { width: number }) => {
   );
 };
 
-function ChartContainer() {
+
+function ChartContainer({ selectedSensor, resolution }: { selectedSensor: Sensor, resolution: string }) {
   const containerRef = useRef<HTMLDivElement>(null);
   const [containerWidth, setContainerWidth] = useState(0);
 
@@ -142,7 +268,9 @@ function ChartContainer() {
 
   return (
     <div ref={containerRef}>
-      {containerWidth > 0 && <DataComponent width={containerWidth} />}
+      {containerWidth > 0 && selectedSensor && (
+        <DataComponent width={containerWidth} selectedSensor={selectedSensor} resolution={resolution} />
+      )}
     </div>
   );
 }
@@ -151,23 +279,35 @@ const LoadingSpinner = () => {
   return <div>Loading...</div>;
 };
 
-export default function TimeseriesChart() {
+export default function TimeseriesChart({ selectedSensor }: { selectedSensor: Sensor }) {
   const { reset } = useQueryErrorResetBoundary();
+  const [resolution, setResolution] = useState("");
+
   return (
-    <ErrorBoundary
-      onReset={reset}
-      fallbackRender={({ resetErrorBoundary }) => (
-        <div className="flex flex-col gap-2 items-center p-4 bg-slate-100 rounded-lg h-96 justify-center">
-          <h3 className="text-lg">
-            Unfortunately there was an error fetching the data!
-          </h3>
-          <Button onClick={() => resetErrorBoundary()}>Try again</Button>
-        </div>
-      )}
-    >
-      <Suspense fallback={<LoadingSpinner />}>
-        <ChartContainer />
-      </Suspense>
-    </ErrorBoundary>
+    <div>
+      <div className="flex items-center justify-between px-1 -mb-1 mt-4">
+        <CardTitle>Sensor Data</CardTitle>
+        <ChartMenu resolution={resolution} setResolution={setResolution} />
+      </div>
+      <ErrorBoundary
+        onReset={reset}
+        fallbackRender={({ resetErrorBoundary }) => (
+          <div className="flex flex-col gap-2 items-center p-4 bg-slate-100 rounded-lg h-96 justify-center">
+            <h3 className="text-lg">
+              Unfortunately there was an error fetching the data!
+            </h3>
+            <Button onClick={() => resetErrorBoundary()}>Try again</Button>
+          </div>
+        )}
+      >
+        <Suspense fallback={<LoadingSpinner />}>
+          {selectedSensor ? (
+            <ChartContainer selectedSensor={selectedSensor} resolution={resolution} />
+          ) : (
+            <div>No sensor selected</div>
+          )}
+        </Suspense>
+      </ErrorBoundary>
+    </div>
   );
 }
