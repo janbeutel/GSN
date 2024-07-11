d25545fa38af029fc538168be6c53c5ec2eeec93
Manuel <manuel.buchauer@student.uibk.ac.at>
2024-07-11 17:47:44 +0200

    del(gsn-webui): delete temperature-rock chart and route

--- gsn-webui/src/app/page.tsx
+++ gsn-webui/src/app/page.tsx
@@ -1,11 +1,5 @@
 import DisplacementChart from "@/components/displacement-chart";
-import TemperatureRockChart from "@/components/temperature-rock-chart";
 
 export default function Home() {
-  return (
-    <ul>
-      <DisplacementChart />;
-      <TemperatureRockChart />;
-    </ul>
-  )
+  return <DisplacementChart />;
 }
